603ceba9f88ef043aafb4ef14fbae21d618aa74c
Mahendra Balu Pawar <mahendrapawar444666@gmail.com>
2022-04-01 15:38:35 +0530

    Api fetch data

--- src/backend/db/categories.js
+++ src/backend/db/categories.js
@@ -1,27 +1,40 @@
 import { v4 as uuid } from "uuid";
 
-/**
- * Category Database can be added here.
- * You can add category of your wish with different attributes
- * */
-
 export const categories = [
   {
     _id: uuid(),
-    categoryName: "fiction",
-    description:
-      "literature in the form of prose, especially novels, that describes imaginary events and people",
+    name:"Sneakers",
+    image:"https://images.unsplash.com/photo-1587563871167-1ee9c731aefb?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MjB8fHNob2VzfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60"
   },
   {
     _id: uuid(),
-    categoryName: "non-fiction",
-    description:
-      "Non-fiction is writing that gives information or describes real events, rather than telling a story.",
+    name:"Casual Shoes",
+    image:"https://cdn.pixabay.com/photo/2016/11/19/18/06/feet-1840619__480.jpg",
   },
   {
     _id: uuid(),
-    categoryName: "horror",
-    description:
-      "Meant to cause discomfort and fear for both the character and readers, horror writers often make use of supernatural and paranormal elements in morbid stories that are sometimes a little too realistic.",
+    name:"Sports Shoes",
+    image:"https://media.istockphoto.com/photos/blue-sneakers-isolated-on-white-background-picture-id1308274455?b=1&k=20&m=1308274455&s=170667a&w=0&h=S3UYmEJ1hUri2rB8jRhJ4wNY31lMrHysLHbl9YD8j_Q="
   },
+  {
+    _id: uuid(),
+    name:"Formal Shoes",
+    image:"https://cdn.pixabay.com/photo/2020/02/28/10/44/shoes-4887100__340.jpg"
+  },
+  {
+    _id: uuid(),
+    name:"Sandals",
+    image:"https://images.unsplash.com/photo-1515347619252-60a4bf4fff4f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NzV8fHNob2VzfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60"
+  },
+  {
+    _id: uuid(),
+    name:"Flip Flops",
+    image:"https://media.istockphoto.com/photos/summer-flipflops-and-sunglasses-isolated-on-white-background-picture-id1144580418?k=20&m=1144580418&s=612x612&w=0&h=JjetNKxrWqGNzcYVKkEYLga3Vo_Re84PgwjlXHKxpCs="
+  },
+  {
+    _id: uuid(),
+    name:"Socks",
+    image:"https://cdn.pixabay.com/photo/2015/04/09/19/56/sock-715024__340.jpg"
+  },
+  
 ];
